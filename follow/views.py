3e2b29fa69197a273898a7ecf72d476f68b55823
pansg <1224439151@qq.com>
2019-07-01 23:08:28 +0800

    完成了follow接口

--- follow/views.py
+++ follow/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def follow(request):
+    return render(request,'follow.html',{})
+def unfollow(request):
+    return render(request,'unfollow.html',{})
